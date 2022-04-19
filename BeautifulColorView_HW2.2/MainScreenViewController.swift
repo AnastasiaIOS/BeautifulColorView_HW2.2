63125bd5ae1477dce689d6cee58ad80d0a974337
Anastasia <pavlova.a.s@inbox.ru>
2022-04-19 22:37:28 +0300

    фвв

---
+++ BeautifulColorView_HW2.2/MainScreenViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainScreenViewController.swift
+//  BeautifulColorView_HW2.2
+//
+//  Created by Anastasia on 19.04.2022.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
