c2867b326542e141babaf1315802c68b0f2b7715
Anastasia <pavlova.a.s@inbox.ru>
2022-04-20 22:34:52 +0300

    add

---
+++ BeautifulColorView_HW2.2/FirstScreenViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstScreenViewController.swift
+//  BeautifulColorView_HW2.2
+//
+//  Created by Anastasia on 20.04.2022.
+//
+
+import UIKit
+
+class FirstScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(
+            red: CGFloat(0.5),
+            green: CGFloat(0.5),
+            blue: CGFloat(0.5),
+            alpha: 1)
+        
+    }
+    
+
+    
+
+}
